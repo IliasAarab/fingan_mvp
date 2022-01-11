9bd9e38284bae104ee06f4232000c17821e4a92e
Ilias Aarab <ilias_aarab@hotmail.com>
2022-01-12 00:20:34 +0100

    Add multivariate gaussian with corr structure

--- IRRBB/src/utils/toydata_generator.py
+++ IRRBB/src/utils/toydata_generator.py
@@ -18,6 +18,14 @@ class dataGenerator:
             loc = kwargs["loc"] if "loc" in kwargs.keys() else 0
             scale = kwargs["scale"] if "scale" in kwargs.keys() else 1
             df = np.random.normal(loc, scale, size=(nsamples, ndim))
+        if distribution.lower() in [
+            "multivariate gaussian",
+            "multi gaussian",
+            "multi_gaussian",
+        ]:
+            loc = kwargs["loc"] if "loc" in kwargs.keys() else 0
+            scale = kwargs["scale"] if "scale" in kwargs.keys() else 1
+            df = np.random.multivariate_normal(loc, scale, size=nsamples)
 
         # Reformat data
         df = pd.DataFrame(df, columns=[f"feature_{i}" for i in range(1, ndim + 1)])
