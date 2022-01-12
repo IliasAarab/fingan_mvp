9ea43344c61f3ca246889efd4f3ff3025ea71946
Ilias Aarab <69244507+IliasAarab@users.noreply.github.com>
2022-01-12 17:39:30 +0100

    Added first part of the GP
    computing the gradient vector dD_dPrg with Prg an interpolated sample between Pr and Pg

--- IRRBB/src/wgan_gp.py
+++ IRRBB/src/wgan_gp.py
@@ -31,15 +31,31 @@ class WGANGP(tfk.Model):
 
     # Compute GP 
     def get_GP(self, batch_n, Pr, Pg):
-
+        
+        ## Sidenote: Steps 1-5 are applied on _each_ interpolated sample but the computations have been vectorized for efficiency
+        # this also mean that batchnormalization isn't allowed anymore within D, as this uses operations on the batch level whereas the GP
+        # is computed on the individual sample level. Batchnorm variations can be possibly be added if they do not introduce correlations between 
+        # individual samples.
+        
         # Step 1: Get interpolated sample between Pr and Pg (`x_hat` of Eq.3)
-        # x_hat= u*Pr +
-        alpha= tf.random.normal(shape= (batch_n, 1), mean= 0, stddev= 1)
-        diff= Pr - Pg
-        Prg= Pr + alpha * diff
+        # x_hat= u*Pr + (1-u)*Pg with u~U(0,1)
+        u= tf.random.uniform(shape= (batch_n, 1), minval=0, maxval=1)
+        Prg= u*Pr + (1-u)*Pg
 
-        Pr + 0.5 * (Pr - Pg)= (1+N)*Pr - N*Pg
-
+        # Step 2: Compute Ds output based on the interpolated batch (while taping the computations)
+        with tf.GradientTape() g_tape:
+            # Watch the interpolation so we can compute gradient wrt interpolated images (and not Ds weights!)
+            # https://www.tensorflow.org/api_docs/python/tf/GradientTape
+            g_tape.watch(Prg)
+            # Forward propogate the interpolation through D
+            D_pred_Prg= D(Prg, training= True) #equivalent but faster than D.predict(Prg) unless multiple batches are used
+        
+        # Step 3: Compute the gradients of Ds weights with respect to the interpolated batch
+        # so D_pred_Prg = D(Prg) and we need dD(Prg)/dPrg 
+        dD_dPrg= g_tape.gradient(D_pred_Prg, [Prg])
+        
+        # Step 4: Compute the norm of the gradient vector
+        
 
 
 
