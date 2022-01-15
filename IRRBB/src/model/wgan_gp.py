5257561ee7da017b7976f408985a8e2979c6b3c3
Ilias Aarab <ilias_aarab@hotmail.com>
2022-01-15 12:53:22 +0100

    adjustments for wgangp. Started to override train_step of  .fit()

---
+++ IRRBB/src/model/wgan_gp.py
@@ -0,0 +1,82 @@
+"""Wasserstein Generative Adversarial Model with Gradient Penalty to ensure 1-Lipschitz continuous Discriminator"""
+
+# Main libs
+import tensorflow as tf
+import tensorflow.keras as tfk
+import numpy as np
+import pandas as pd
+
+# In-house modules
+from .gan_base import GANBase
+from ..utils.parser import Parser
+
+
+class WGANGP(tfk.Model):
+
+    # Add to keras og init method
+    def __init__(self, D, G, D_n=5, lambda_=10):
+
+        super().__init__()
+        self.D = D
+        self.G = G
+        self.D_n = D_n
+        self.lambda_ = lambda_
+
+    # Add to keras og compile method
+    def compile(self, D_optimizer, G_optimizer, D_loss, G_loss):
+
+        super.compile()
+        self.D_optimizer = D_optimizer
+        self.G_optimizer = G_optimizer
+        self.D_loss = D_loss
+        self.G_loss = G_loss
+
+    # Compute GP
+    def get_GP(self, Pr, Pg):
+
+        ## Sidenote: Steps 1-5 are applied on _each_ interpolated sample but the computations have been vectorized for efficiency
+        # this also mean that batchnormalization isn't allowed anymore within D, as this uses operations on the batch level whereas the GP
+        # is computed on the individual sample level. Batchnorm variations can possibly be added if they do not introduce correlations between
+        # individual samples.
+
+        # Step 1: Get interpolated sample between Pr and Pg (`x_hat` of Eq.3)
+        # x_hat= u*Pr + (1-u)*Pg with u~U(0,1)
+        u = tf.random.uniform(shape=(Pr.shape[0], 1), minval=0, maxval=1)
+        Prg = u * Pr + (1 - u) * Pg
+
+        # Step 2: Compute Ds output based on the interpolated batch (while taping the computations)
+        with tf.GradientTape() as g_tape:
+            # Watch the interpolation so we can compute the gradient wrt the interpolated images (and not Ds weights!)
+            # https://www.tensorflow.org/api_docs/python/tf/GradientTape
+            g_tape.watch(Prg)
+            # Forward propogate the interpolation through D
+            D_pred_Prg = self.D(
+                Prg, training=True
+            )  # equivalent but faster than D.predict(Prg) unless multiple batches are used
+
+        # Step 3: Compute the gradients of Ds weights with respect to the interpolated batch
+        # so D_pred_Prg = D(Prg) and we need dD(Prg)/dPrg
+        dD_dPrg = g_tape.gradient(D_pred_Prg, Prg)
+
+        # Step 4: Compute the norm of the gradient vector
+        norm_dD_dPrg = tf.sqrt(tf.reduce_sum(tf.square(dD_dPrg), axis=1))
+
+        # Step 5: Compute the gradient penalty as described in Eq.3 of the paper
+        gp = tf.reduce_mean(tf.square(norm_dD_dPrg - 1))
+
+        return gp
+
+    
+    # Override Tensorflow's train_step() method, this is the method that is being called for each batch when
+    # running .fit(). We are going to recreate this method to define the unique way GANs are trained
+    def train_step(self, Pr):
+
+        # Step 1: Train D until convergence / pre-determined number of steps
+        # TODO: create convergence option
+        for each_step in self.D_n:
+            
+            # Step 1.1: Create ml tuple (X, y)
+            # Step 1.1.1: Generate fake samples from Generator in current trained state
+
+            #self.D.fit(x= Pr, y= ytrain, epochs= 10)
+
