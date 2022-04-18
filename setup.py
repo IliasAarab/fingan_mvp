74d72735cae4e688553c39525873b30c725add8f
Aarab, Ilias <Ilias.Aarab@ecb.europa.eu>
2022-04-18 20:29:58 +0200

    packaging

--- setup.py
+++ setup.py
@@ -29,5 +29,5 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "License :: OSI Approved :: MIT License",
     ],
-    package_dir={"":"IRRBB/src"},
-    packages=find_packages(where= "IRRBB/src") )
+    package_dir={"":"IRRBB"},
+    packages=find_packages(where= "src") )
